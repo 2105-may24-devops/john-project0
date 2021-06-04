ab29c23e8debd33b490a7c070bf86ed97d17a272
John Drake <jdrake@uoregon.edu>
2021-06-04 11:22:13 -0500

    Emulating changes made by my partner with whitespace and newlines, this is a new branch.

--- cryptotrading.py
+++ cryptotrading.py
@@ -6,16 +6,16 @@ import sys
 
 def getbuyinformation():
 	f = open("cryptobuy.txt", "a")
-	crypto = input("Which Cryptocurrency is being purchased? ")
-	print("\n")
-	cryptoamount = input("How Much " + crypto + " did you purchase? ")
-	print("\n")
-	usdamount = input("How much USD did the " + cryptoamount + "  " + crypto + " cost you in this buy? ")
-	print("\n")
-	exchangerate = input("How much USD is 1.0 " + crypto + " currently? ")
-	print("\n")
-	notes = input("Any notes on why you are making this buy of " + cryptoamount + " " + crypto + " today? REMEMBER: Don't invest more than you're willing to lose. Never feel guilty, if you feel like you have a problem there is plenty of help out there . Everyone has experiences with loss. Someone spent 10,000 BTC on a pizza when the exchange rate was low. Everything will be ok no matter how the market treats you :)")
-	print("\n")
+	crypto = input("Which Cryptocurrency is being purchased? \n")
+	#print("\n")
+	cryptoamount = input("How Much " + crypto + " did you purchase? \n")
+	#print("\n")
+	usdamount = input("How much USD did the " + cryptoamount + "  " + crypto + " cost you in this buy? \n")
+	#print("\n")
+	exchangerate = input("How much USD is 1.0 " + crypto + " currently? \n")
+	#print("\n")
+	notes = input("Any notes on why you are making this buy of " + cryptoamount + " " + crypto + " today? REMEMBER: Don't invest more than you're willing to lose. Never feel guilty, if you feel like you have a problem there is plenty of help out there . Everyone has experiences with loss. Someone spent 10,000 BTC on a pizza when the exchange rate was low. Everything will be ok no matter how the market treats you :) \n")
+	#print("\n")
 	f.write("Bought " + cryptoamount + " " + crypto + "\n")
 	f.write("The purchase of " + cryptoamount + " " + crypto + " cost " + usdamount  + " USD." + "\n")
 	f.write("The Exchange rate at the time of this sale was 1.0 " + crypto + "=" + exchangerate + " USD." + "\n")
@@ -24,19 +24,20 @@ def getbuyinformation():
 
 
 def getsellinformation():
+
 	f = open("cryptosell.txt", "a")
 	
 	
-	crypto = input("Which Cryptocurrency is being sold? ")
-	print("\n")
-	cryptoamount = input("How Much " + crypto + " are you selling?")
-	print("\n")
-	usdamount = input("How much USD did you receive from selling your " + cryptoamount + " " + crypto + " " + " in this sell order? ")
-	print("\n")
-	exchangerate = input("How much USD is 1.0 " + crypto + " currently? ")
-	print("\n")
-	notes = input("Any notes on why you are selling " + cryptoamount + "of your " + crypto + " today? REMEMBER: Hold if you can, but if you need the money, it's ok :) ")
-	print("\n")
+	crypto = input("Which Cryptocurrency is being sold? \n")
+	
+	cryptoamount = input("How Much " + crypto + " are you selling? \n")
+	#print("\n")
+	usdamount = input("How much USD did you receive from selling your " + cryptoamount + " " + crypto + " " + " in this sell order? \n")
+	#print("\n")
+	exchangerate = input("How much USD is 1.0 " + crypto + " currently? \n")
+	#print("\n")
+	notes = input("Any notes on why you are selling " + cryptoamount + "of your " + crypto + " today? REMEMBER: Hold if you can, but if you need the money, it's ok :) \n")
+	#print("\n")
 	f.write("Sold " + cryptoamount + " " + crypto + "\n")
 	f.write("The sale of " + cryptoamount + " " + crypto + " netted " + usdamount  + " USD." + "\n")
 	f.write("The Exchange rate at the time of this sale was 1.0 " + crypto + "=" + exchangerate + " USD." + "\n")
@@ -54,7 +55,7 @@ if __name__ == "__main__":
     #data = response.json()
     #print(data["bpi"]["USD"]["rate"])
     while(True):
-    	buyorsell = input("Is this a record of a buy or a sell order? Type 'q' to quit. ")
+    	buyorsell = input("Is this a record of a buy or a sell order? Type 'q' to quit. \n")
     	if buyorsell == "buy":
     		getbuyinformation()
     	elif buyorsell == "sell":
@@ -62,4 +63,4 @@ if __name__ == "__main__":
     	elif buyorsell == "q":
     		break
     	else:
-    		print("Only 'buy' or 'sell' orders are are accepted, along with the quit command 'q'. please enter one of the three.")
+    		print("Only 'buy' or 'sell' orders are are accepted, along with the quit command 'q'. please enter one of the three. \n")
