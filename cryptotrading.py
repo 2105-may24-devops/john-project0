dd32ef488981ec60b7813c1a3846f05e3b809a25
John Drake <jdrake@uoregon.edu>
2021-06-08 11:35:05 -0500

    Got rid of some more wierd stuff, fixed ugly spacing between NEW BUY/SELL record lines in output file, added date and time to be written to output file

--- cryptotrading.py
+++ cryptotrading.py
@@ -1,35 +1,39 @@
 import sys
+import time
 #import requests
 
 
 
 # Function called for buying cryptocurrency and logging the sale
-def getbuyinformation():
+def getbuyinformation(curtime):
 	#If this is the first recorded buy, create a log file for buy orders. If the program has been used to record buys before, add another entry to this file. It is opened here.
 	f = open("cryptobuy.txt", "a")
 	crypto = input("Which Cryptocurrency is being purchased? \n")
 
-	#print("\n")
+	
 	cryptoamount = input("How Much " + crypto + " did you purchase? \n")
-	#print("\n")
+	
 	usdamount = input("How much USD did the " + cryptoamount + " " + crypto + " cost you in this buy? \n")
-	#print("\n")
+	
 	exchangerate = input("How much USD is 1.0 " + crypto + " currently? \n")
-	#print("\n")
+	
 	notes = input("Any notes on why you are making this buy of " + cryptoamount + " " + crypto + " today? REMEMBER: Don't invest more than you're willing to lose. Never feel guilty, if you feel like you have a problem there is plenty of help out there . Everyone has experiences with loss. Someone spent 10,000 BTC on a pizza when the exchange rate was low. Everything will be ok no matter how the market treats you :) \n")
-	#print("\n")
+	
 
 
 
 	#Write to cryptobuy.txt with the information of the buy
+	f.write("\n" + "\n" + "NEW BUY RECORD" + "\n" + "\n")
+	f.write(time.strftime('%H:%M%p %Z on %b %d %Y'))
+	f.write("\n")
 	f.write("Bought " + cryptoamount + " " + crypto + "\n")
 	f.write("The purchase of " + cryptoamount + " " + crypto + " cost " + usdamount  + " USD." + "\n")
 	f.write("The Exchange rate at the time of this sale was 1.0 " + crypto + "=" + exchangerate + " USD." + "\n")
-	f.write("Notes: " + notes + "\n" + "\n" + "NEW BUY RECORD" + "\n" + "\n")
+	f.write("Notes: " + notes +"\n" + "\n")
 	f.close()
 
 # Function called for selling cryptocurrency and logging the sale
-def getsellinformation():
+def getsellinformation(curtime):
 	#Create a log file for sales if it doesn't exist, or open it and continue writing for it if the program has previously been ran for a sell order
 	f = open("cryptosell.txt", "a")
 
@@ -38,21 +42,24 @@ def getsellinformation():
 	crypto = input("Which Cryptocurrency is being sold? \n")
 	
 	cryptoamount = input("How Much " + crypto + " did you sell? \n")
-	#print("\n")
+	
 	usdamount = input("How much USD did you receive from selling your " + cryptoamount + " " + crypto + " " + " in this sell order? \n")
-	#print("\n")
+	
 	exchangerate = input("How much USD is 1.0 " + crypto + " currently? \n")
-	#print("\n")
-	notes = input("Any notes on why you are selling " + cryptoamount + "of your " + crypto + " today? REMEMBER: Hold if you can, but if you need the money, it's ok :) \n")
-	#print("\n")
+	
+	notes = input("Any notes on why you are selling " + cryptoamount + " of your " + crypto + " today? REMEMBER: Hold if you can, but if you need the money, it's ok :) \n")
+	
 
 
 
 	#Write to cryptosell.txt with information of this sell order
+	f.write("NEW SELL RECORD" + "\n" + "\n")
+	f.write(time.strftime('%H:%M%p %Z on %b %d %Y'))
+	f.write("\n")
 	f.write("Sold " + cryptoamount + " " + crypto + "\n")
 	f.write("The sale of " + cryptoamount + " " + crypto + " netted " + usdamount  + " USD." + "\n")
 	f.write("The Exchange rate at the time of this sale was 1.0 " + crypto + "=" + exchangerate + " USD." + "\n")
-	f.write("Notes: " + notes + "\n" + "\n" + "NEW SELL RECORD" + "\n" + "\n")
+	f.write("Notes: " + notes+"\n" + "\n")
 	f.close()
 
 
@@ -62,12 +69,13 @@ if __name__ == "__main__":
     #data = response.json()
     #print(data["bpi"]["USD"]["rate"])
     while(True):
+    	curtime = time.ctime()
     	buyorsell = input("Is this a record of a buy or a sell order? Type 'q' to quit. \n")
     	if buyorsell == "buy":
-    		getbuyinformation()
+    		getbuyinformation(curtime)
     		continue
     	elif buyorsell == "sell":
-    		getsellinformation()
+    		getsellinformation(curtime)
     		continue
     	elif buyorsell == "q":
     		break
